f2f2a8fefa58352f4bad5c32fb95dc4ccd323294
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-12 10:44:51 +0330

    Add eisode 6 chapter 2

---
+++ S2_blog_application_(part_one)/P5_crate_panel_admin.py
@@ -0,0 +1,3 @@
+# Create a superuser
+# Register the Post model
+# Create and edit a post and user/
---
+++ S2_blog_application_(part_one)/P6_admin_panel_customization.py
@@ -0,0 +1,9 @@
+# list_display
+# ordering
+# list_filter
+# search_fields
+# raw_id_fields
+# date_hierarchy
+# prepopulated_fields
+# list_editable
+# list_display_links
