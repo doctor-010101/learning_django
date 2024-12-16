82cac01a97f99fa4115f9c6e8454b6315df9b8df
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-16 07:49:58 +0330

    Add eisode 8 chapter 2

---
+++ S2_blog_application_(part_one)/P8_ORM_CRUD_part_two.py
@@ -0,0 +1,8 @@
+# Working with all
+# Working with filter
+# Working with exclude
+# Working with order_by
+# Working with delete
+# Working with get
+# Working with update
+# When are lazy query sets evaluated?
