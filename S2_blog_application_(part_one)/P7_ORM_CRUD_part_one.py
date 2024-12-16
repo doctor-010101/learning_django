5fa6a9c1e55e7aa346b8a42c475da2223a98c519
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-16 07:47:47 +0330

    Add eisode 7 chapter 2

---
+++ S2_blog_application_(part_one)/P7_ORM_CRUD_part_one.py
@@ -0,0 +1,7 @@
+# Introduction to the concept of ORM
+# Introduction to the concept of CRUD in the database
+# Introduction to the concept of QuerySet
+# Introduction to the concept of Manager
+# Working with get
+# Creating a new object (record)
+# Save method
