ed843ca338a6a8bfd775e2d28afaebab56afe3a6
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-24 12:08:56 +0330

    Add eisode 21 chapter 2

---
+++ S2_blog_application_(part_one)/P12_create_a_base_template.py
@@ -0,0 +1,6 @@
+# Introduction to Django template language
+# Introduction to template variable
+# Introduction to template filter
+# Introduction to template tag
+# Introduction to static, block, and include tags
+# Creating a base template
