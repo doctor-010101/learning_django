c916ab97a82a9ac14fa7ab8c86547cf93acaae20
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-25 19:06:28 +0330

    Add eisode 13 chapter 2

---
+++ S2_blog_application_(part_one)/P13_create_list_and_detail_templates.py
@@ -0,0 +1,7 @@
+# Create a list view template
+# Create a detail view template
+# Learn about the extends tag
+# Learn about the url tag
+# Learn about the truncatewords filter
+# Learn about the linebreaks filter
+# Learn about the for tag
