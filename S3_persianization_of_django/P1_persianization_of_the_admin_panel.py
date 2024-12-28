f43484f3ca32771a6541f605d0b62ac6af6e0106
Hassan <hassan.cod.dev.0101@gmail.com>
2024-12-28 20:53:43 +0330

    Add eisode 1 chapter 3

---
+++ S3_persianization_of_django/P1_persianization_of_the_admin_panel.py
@@ -0,0 +1,5 @@
+# Persianize the application name in the admin
+# Persianize the admin header and title
+# Persianize the model name in the admin
+# Persianize the field names in the admin
+# Persianize all the admin options and entries
