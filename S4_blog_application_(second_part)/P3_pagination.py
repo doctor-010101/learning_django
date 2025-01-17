9227f7f14cec89528dc5c1096b2b8ffb4aa518f3
Hassan <hassan.cod.dev.0101@gmail.com>
2025-01-17 09:39:53 +0330

    Add episode 3 chapter 4

---
+++ S4_blog_application_(second_part)/P3_pagination.py
@@ -0,0 +1,9 @@
+# List Pagination with Pagination
+# Creating Template Pagination
+# Learning about has_previous, page.previous_page_number, page.number, page.paginator.page_range, page.paginator.num_pages, page.has_next, page.next_page_number,
+# Learning about in template tag
+# Pagination theme changer
+# Handling EmptyPage exception
+# Handling PageNotAnInteger exception
+# Fixing style issues
+# Learning about filter lengthJ
