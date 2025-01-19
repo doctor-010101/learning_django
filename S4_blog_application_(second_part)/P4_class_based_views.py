71e3e5c8b09c376e52d25e564ff943902c379c0d
Hassan <hassan.cod.dev.0101@gmail.com>
2025-01-19 18:52:46 +0330

    Add episode 4 chapter 4

---
+++ S4_blog_application_(second_part)/P4_class_based_views.py
@@ -0,0 +1,6 @@
+# Introduction to class-based views
+# Creating a view for a list post with ListView
+# Creating a view for a detail post with DetailView
+# Modifying URLs
+# Working with queryset, context_object_name, paginate_by, template_name, model attributes
+# Changing the pagination variable to page_obj
