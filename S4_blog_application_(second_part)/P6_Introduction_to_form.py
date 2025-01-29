5db47c92163dba32bd59fbfbabe1ba0094e0114d
Hassan <hassan.cod.dev.0101@gmail.com>
2025-01-29 22:04:06 +0330

    Add episode 6 chapter 4

---
+++ S4_blog_application_(second_part)/P6_Introduction_to_form.py
@@ -0,0 +1,14 @@
+# Create a forms.py file
+# Create a ticket model
+# Use the new EmailField field
+# Learn about the types of fields in a form
+# Create a form for a ticket
+# Create a URL for a ticket
+# Create a view for a ticket
+# Work with a form in a view
+# Learn about the save method
+# Learn about the create method
+# Create a ticket template
+# Learn about as_p
+# Learn about the concept of csrf token
+# Show the ticket model in the admin
