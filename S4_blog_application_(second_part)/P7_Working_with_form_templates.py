bd5d0706f5dc409ef8656811d95d8f9b31cf8347
Hassan <hassan.cod.dev.0101@gmail.com>
2025-02-10 15:04:18 +0330

    Add episode 7 chapter 4

---
+++ S4_blog_application_(second_part)/P7_Working_with_form_templates.py
@@ -0,0 +1,6 @@
+# Introduction to the request.POST structure
+# Introduction to the csrf token tag
+# Setting field values ​​with create
+# Introduction to form rendering options
+# Looping over the form
+# Handwriting tags
