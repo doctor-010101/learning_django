e46e35c55b466dac42d932c21821fc2b6094d669
Hassan <hassan.cod.dev.0101@gmail.com>
2025-02-22 20:37:40 +0330

    Add episode 8 chapter 4

---
+++ S4_blog_application_(second_part)/P8_validating_forms_displaying_errors.py
@@ -0,0 +1,5 @@
+# It looks like the code snippet you provided is a comment in a Python file that outlines the tasks
+# related to form validation in a web application. Here is a breakdown of each task:
+Show errors in template
+Show field value after error
+Form validation
