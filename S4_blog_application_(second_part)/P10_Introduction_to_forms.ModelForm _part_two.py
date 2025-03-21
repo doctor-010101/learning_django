77d89eaa187e455843b07905fc8e06a90dd6031a
Hassan <hassan.cod.dev.0101@gmail.com>
2025-03-21 16:00:16 +0330

    Add episode 10 chapter 4

---
+++ S4_blog_application_(second_part)/P10_Introduction_to_forms.ModelForm _part_two.py
@@ -0,0 +1,8 @@
+# Creating a URL for a comment
+# Creating a template for a comment form
+# Creating a form on the details page
+# Learning about the pluralize filter
+# Learning about the with tag template
+# Learning about the concept of db hits
+# Learning about empty in the for tag template
+# Writing validation for the form model
